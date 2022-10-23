15b166e7607ffb782798dcea458c513acac34391
Def <deferenseo@gmail.com>
2022-10-23 14:09:39 +0300

    install slick-carousel

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,21 @@
-console.log('Lorem');
+$(function() {
+    $('.products__slider').slick({
+        infinite: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                dots: true
+            }
+        }, {
+            breakpoint: 300,
+            settings: "unslick"
+        }]
+    });
+});
